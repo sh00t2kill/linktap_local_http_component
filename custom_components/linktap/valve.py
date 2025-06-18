35c7bd99c57fa95537e6187ba92fef1c98623211
Dan Wheaton <dan@DESKTOP-FDSRCUR.localdomain>
2025-06-18 11:20:37 +1000

    Allow pause to accept 0 as a value, which disables the pause

--- custom_components/linktap/valve.py
+++ custom_components/linktap/valve.py
@@ -142,8 +142,8 @@ class LinktapValve(CoordinatorEntity, ValveEntity):
     def device_info(self) -> DeviceInfo:
         return self._attr_device_info
 
-    async def _pause_tap(self, hours=False):
-        if not hours:
+    async def _pause_tap(self, hours=None):
+        if hours is None:
             hours = 1
         _LOGGER.debug(f"Pausing {self.entity_id} for {hours} hours")
         gw_id = self.coordinator.get_gw_id()
