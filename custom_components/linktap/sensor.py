4b7a860767c8d5c903bf94f43434326cb2c034d7
sh00t2kill <dan@dwheaton.org>
2026-08-12 14:40:42 +1000

    added total cumulative sensors for duration and volume

--- custom_components/linktap/sensor.py
+++ custom_components/linktap/sensor.py
@@ -7,9 +7,10 @@ import aiohttp
 import homeassistant.helpers.config_validation as cv
 import homeassistant.util.dt as dt_util
 import voluptuous as vol
-from homeassistant.components.sensor import SensorEntity
+from homeassistant.components.sensor import SensorEntity, SensorStateClass
 from homeassistant.helpers.entity import *
 from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.restore_state import RestoreEntity
 from homeassistant.helpers.update_coordinator import (CoordinatorEntity,
                                                       DataUpdateCoordinator)
 from homeassistant.util import slugify
@@ -34,9 +35,11 @@ async def async_setup_entry(
         sensors.append(LinktapSensor(coordinator, hass, tap, data_attribute="battery", unit="%", device_class="battery"))
         sensors.append(LinktapSensor(coordinator, hass, tap, data_attribute="total_duration", unit="s", icon="mdi:clock"))
         sensors.append(LinktapSensor(coordinator, hass, tap, data_attribute="remain_duration", unit="s", icon="mdi:clock"))
+        sensors.append(LinktapWateringTimeTotalSensor(coordinator, hass, tap))
         sensors.append(LinktapSensor(coordinator, hass, tap, data_attribute="speed", unit=f"{vol_unit}pm", icon="mdi:speedometer"))
         sensors.append(LinktapSensor(coordinator, hass, tap, data_attribute="volume", unit=vol_unit, device_class="water", icon="mdi:water-percent"))
         sensors.append(LinktapSensor(coordinator, hass, tap, data_attribute="volume_limit", unit=vol_unit, icon="mdi:water-percent"))
+        sensors.append(LinktapVolumeTotalSensor(coordinator, hass, tap, unit=vol_unit))
         sensors.append(LinktapSensor(coordinator, hass, tap, data_attribute="failsafe_duration", unit="s", icon="mdi:clock"))
         sensors.append(LinktapSensor(coordinator, hass, tap, data_attribute="plan_mode", unit="mode", icon="mdi:note"))
         sensors.append(LinktapSensor(coordinator, hass, tap, data_attribute="plan_sn", unit="sn", icon="mdi:note"))
@@ -54,8 +57,6 @@ class LinktapSensor(CoordinatorEntity, SensorEntity):
         self.attribute = data_attribute
         self.tap_id = tap[TAP_ID]
         self.tap_name = tap[NAME]
-        #if data_attribute == "volume":
-        #    self.last_reset = dt_util.utcnow()
         self.platform = "sensor"
         self._attr_unique_id = slugify(f"{DOMAIN}_{self.platform}_{data_attribute}_{self.tap_id}")
         self._attrs = {
@@ -66,7 +67,7 @@ class LinktapSensor(CoordinatorEntity, SensorEntity):
         if device_class:
             self._attr_device_class = device_class
             if device_class == "water":
-                self._attr_state_class = "total"
+                self._attr_state_class = SensorStateClass.MEASUREMENT
 
         self._attr_device_info = DeviceInfo(
             identifiers={
@@ -109,11 +110,105 @@ class LinktapSensor(CoordinatorEntity, SensorEntity):
                 self._state = self.translate_plan_mode(attributes["plan_mode"])
             else:
                 self._state = attributes[self.attribute]
-            if previous_state and self.attribute == "volume" and self._state < previous_state:
-                self.last_reset = dt_util.utcnow()
 
         return self._state
 
     @property
     def device_info(self) -> DeviceInfo:
         return self._attr_device_info
+
+
+class LinktapVolumeTotalSensor(CoordinatorEntity, RestoreEntity, SensorEntity):
+
+    def __init__(self, coordinator, hass, tap, unit):
+        super().__init__(coordinator)
+        self.tap_id = tap[TAP_ID]
+        self._attr_name = f"{MANUFACTURER} {tap[NAME]} Volume Total"
+        self._attr_unique_id = slugify(f"{DOMAIN}_sensor_volume_total_{self.tap_id}")
+        self._attr_native_unit_of_measurement = unit
+        self._attr_device_class = "water"
+        self._attr_state_class = SensorStateClass.TOTAL_INCREASING
+        self._attr_icon = "mdi:water"
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, tap[TAP_ID])},
+            name=tap[NAME],
+            manufacturer=MANUFACTURER,
+            model=tap[TAP_ID],
+            configuration_url="http://" + tap[GW_IP] + "/"
+        )
+        self._total = 0.0
+        self._previous_volume = 0.0
+
+    async def async_added_to_hass(self):
+        await super().async_added_to_hass()
+        state = await self.async_get_last_state()
+        if state and state.state not in ("unknown", "unavailable"):
+            try:
+                self._total = float(state.state)
+            except ValueError:
+                self._total = 0.0
+
+    def _handle_coordinator_update(self) -> None:
+        if not self.coordinator.data:
+            return
+        current = float(self.coordinator.data.get("volume", 0))
+        # volume drop means a new session started; commit the completed session
+        if current < self._previous_volume:
+            self._total += self._previous_volume
+        self._previous_volume = current
+        self.async_write_ha_state()
+
+    @property
+    def native_value(self):
+        return round(self._total + self._previous_volume, 1)
+
+
+class LinktapWateringTimeTotalSensor(CoordinatorEntity, RestoreEntity, SensorEntity):
+
+    def __init__(self, coordinator, hass, tap):
+        super().__init__(coordinator)
+        self.tap_id = tap[TAP_ID]
+        self._attr_name = f"{MANUFACTURER} {tap[NAME]} Watering Time Total"
+        self._attr_unique_id = slugify(f"{DOMAIN}_sensor_watering_time_total_{self.tap_id}")
+        self._attr_native_unit_of_measurement = "s"
+        self._attr_state_class = SensorStateClass.TOTAL_INCREASING
+        self._attr_icon = "mdi:clock"
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, tap[TAP_ID])},
+            name=tap[NAME],
+            manufacturer=MANUFACTURER,
+            model=tap[TAP_ID],
+            configuration_url="http://" + tap[GW_IP] + "/"
+        )
+        self._total = 0.0
+        self._previous_watering = False
+        self._session_start = None
+
+    async def async_added_to_hass(self):
+        await super().async_added_to_hass()
+        state = await self.async_get_last_state()
+        if state and state.state not in ("unknown", "unavailable"):
+            try:
+                self._total = float(state.state)
+            except ValueError:
+                self._total = 0.0
+
+    def _handle_coordinator_update(self) -> None:
+        if not self.coordinator.data:
+            return
+        is_watering = self.coordinator.data.get("is_watering", False)
+        now = dt_util.utcnow()
+        if is_watering and not self._previous_watering:
+            self._session_start = now
+        elif not is_watering and self._previous_watering and self._session_start:
+            self._total += (now - self._session_start).total_seconds()
+            self._session_start = None
+        self._previous_watering = is_watering
+        self.async_write_ha_state()
+
+    @property
+    def native_value(self):
+        if self._session_start is not None:
+            elapsed = (dt_util.utcnow() - self._session_start).total_seconds()
+            return round(self._total + elapsed, 1)
+        return round(self._total, 1)
